d2af223586c1e8b1e4f965692cb18215069b248b
Michael Crump <mbcrump29@gmail.com>
2020-04-26 12:43:43 -0700

    Add project files.

---
+++ Fruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateJsonValueForYuuandmi
+{
+    public class Products
+    {
+        public string Fruit { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace UpdateJsonValueForYuuandmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products products = new Products { Color = "Purple", Fruit = "Onions", Size = "" };
+
+            // serialize JSON to a string and then write string to a file
+            File.WriteAllText(@"C:\Users\Michael Crump\source\repos\UpdateJsonValueForYuuandmi\example.json", JsonConvert.SerializeObject(products));
+
+            // serialize JSON directly to a file
+            using (StreamWriter file = File.CreateText(@"C:\Users\Michael Crump\source\repos\UpdateJsonValueForYuuandmi\example.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, products);
+            }
+        }
+    }
+}
